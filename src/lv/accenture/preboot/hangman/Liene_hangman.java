d950225286bdecc545b17b93320f485e85d57060
LieneA <liene.apsalone@gmail.com>
2019-11-30 13:38:23 +0200

    mana klase, kurā pārsvarā strādāšu

---
+++ src/lv/accenture/preboot/hangman/Liene_hangman.java
@@ -0,0 +1,40 @@
+package lv.accenture.preboot.hangman;
+
+import java.util.Scanner;
+
+public class Liene_hangman {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String word = "dinozaurs";
+
+
+		char[] symbols = new char[word.length()];
+		for (int i = 0; i < word.length(); i++) {
+			symbols[i] = word.charAt(i);
+		}
+		
+		char[] symbols = new char[word.length()];
+		for (int i = 0; i < word.length(); i++) {
+			symbols[i] = word.charAt(i);
+		}
+		
+		for (int i=0;i<symbols.length;i++) {
+			System.out.print(symbols[i]+" ");
+		}
+		
+		Scanner sc = new Scanner(System.in);
+		char userGuess = sc.next().charAt(0);
+		
+		for (int i=0;i<symbols.length;i++) {
+if (symbols[i]==userGuess){
+	
+}
+		}
+
+	}
+
+}
+
