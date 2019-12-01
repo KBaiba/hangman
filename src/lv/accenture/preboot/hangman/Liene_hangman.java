0522a61265e1d303469b95e3183ef7fe11760f7a
LieneA <liene.apsalone@gmail.com>
2019-12-01 21:24:37 +0200

    -

--- src/lv/accenture/preboot/hangman/Liene_hangman.java
+++ src/lv/accenture/preboot/hangman/Liene_hangman.java
@@ -8,33 +8,57 @@ public class Liene_hangman {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String word = "dinozaurs";
-
+		String word = "triceratops";
+		int i = 0;
+		int countGuessed = 0;
+		int countMistakes = 0;
 
 		char[] symbols = new char[word.length()];
-		for (int i = 0; i < word.length(); i++) {
+		for (i = 0; i < word.length(); i++) {
 			symbols[i] = word.charAt(i);
 		}
-		
+
 		char[] symbols2 = new char[word.length()];
-		for (int i = 0; i < word.length(); i++) {
-			symbols[i] = '*';
+		for (i = 0; i < word.length(); i++) {
+			symbols2[i] = '*';
 		}
-		
-		for (int i=0;i<symbols.length;i++) {
-			System.out.print(symbols[i]+" ");
-		}
-		
-		Scanner sc = new Scanner(System.in);
-		char userGuess = sc.next().charAt(0);
-		
-		for (int i = 0; i < symbols.length; i++) {
-			if (symbols[i] == userGuess) {
-
+		while ((countGuessed != word.length()) && (countMistakes < 10)) {
+			for (i = 0; i < symbols2.length; i++) {
+				System.out.print(symbols2[i] + " ");
 			}
-		}
+			System.out.println("");
+			System.out.println("Please enter a letter!");
 
+			Scanner sc = new Scanner(System.in);
+			char userGuess1 = sc.next().charAt(0);
+			char userGuess=Character.toLowerCase(userGuess1);
+			boolean mistake = true;
+
+			for (i = 0; i < symbols.length; i++) {
+				if ((symbols[i] == userGuess) && (symbols2[i] == userGuess)) {
+					mistake = false;
+				} else if (symbols[i] == userGuess) {
+					symbols2[i] = userGuess;
+					countGuessed = countGuessed + 1;
+					mistake = false;
+				}
+			}
+
+			if (mistake) {
+				countMistakes = countMistakes + 1;
+				// palaist cilvecina funkciju
+				System.out.println("That's not right! You have " + (10 - countMistakes) + " mistakes left!");
+			}
+			System.out.println("Guessed:" + countGuessed);
+			System.out.println("Mistakes: " + countMistakes);
+		}
+		for (i = 0; i < symbols2.length; i++) {
+			System.out.print(symbols2[i] + " ");
+		}
+		System.out.println("");
+		if (countGuessed == word.length()) {
+			System.out.println("Congratulations! You won! :)");
+		}
 	}
 
 }
-
