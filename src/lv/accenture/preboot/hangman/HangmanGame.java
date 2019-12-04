623ff9eea8dfba4a80763c339bbee20056a3e9da
LieneA <liene.apsalone@gmail.com>
2019-12-04 21:12:00 +0200

    -

--- src/lv/accenture/preboot/hangman/HangmanGame.java
+++ src/lv/accenture/preboot/hangman/HangmanGame.java
@@ -11,92 +11,103 @@ public class HangmanGame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		PrintPicture printer = new PrintPicture();
-		WordChooser reader = new WordChooser();
-		String word = reader.getWord();
+		boolean playAgain = true;
+		while (playAgain) {
+			PrintPicture printer = new PrintPicture();
+			WordChooser reader = new WordChooser();
+			String word = reader.getWord();
 
-		int i = 0;
-		int countGuessed = 0;
-		int countMistakes = 0;
-		HashSet<Character> wrongGuess = new HashSet<Character>();
+			int i = 0;
+			int countGuessed = 0;
+			int countMistakes = 0;
+			HashSet<Character> wrongGuess = new HashSet<Character>();
 
-		char[] symbols = new char[word.length()];
-		for (i = 0; i < word.length(); i++) {
-			symbols[i] = word.charAt(i);
-		}
-
-		char[] symbols2 = new char[word.length()];
-		for (i = 0; i < word.length(); i++) {
-			symbols2[i] = '*';
-		}
-		System.out.println("Let the game begin! Here is a word to guess: ");
-		
-		while ((countGuessed != word.length()) && (countMistakes < 8)) {
-			System.out.println("__________________________________________________________________________"+"\n");
-			for (i = 0; i < symbols2.length; i++) {
-				System.out.print(symbols2[i] + " ");
+			char[] symbols = new char[word.length()];
+			for (i = 0; i < word.length(); i++) {
+				symbols[i] = word.charAt(i);
 			}
-			Scanner sc = new Scanner(System.in);
+
+			char[] symbols2 = new char[word.length()];
+			for (i = 0; i < word.length(); i++) {
+				symbols2[i] = '*';
+			}
+			System.out.println("Let the game begin! Here is a word to guess: ");
+
+			while ((countGuessed != word.length()) && (countMistakes < 8)) {
+				System.out.println("__________________________________________________________________________" + "\n");
+				for (i = 0; i < symbols2.length; i++) {
+					System.out.print(symbols2[i] + " ");
+				}
+				Scanner sc = new Scanner(System.in);
+
+				System.out.println("");
+
+				boolean validInput = false;
+				String userGuess2 = " ";
+
+				while (!validInput) {
+					System.out.println("\n" + "Please enter a letter!");
+					if (!wrongGuess.isEmpty()) {
+						System.out.println("You've already tried: " + wrongGuess);
+
+					}
+					userGuess2 = sc.next();
+					if ((userGuess2.matches("[A-Za-z]{1}")) && (userGuess2.length() == 1)) {
+						validInput = true;
+					} else {
+						System.out.println("Wrong input!");
+					}
+
+				}
+
+				char userGuess1 = userGuess2.charAt(0);
+				char userGuess = Character.toLowerCase(userGuess1);
+				boolean mistake = true;
+
+				for (i = 0; i < symbols.length; i++) {
+					if ((symbols[i] == userGuess) && (symbols2[i] == userGuess)) {
+						mistake = false;
+					} else if (symbols[i] == userGuess) {
+						symbols2[i] = userGuess;
+						countGuessed = countGuessed + 1;
+						mistake = false;
+
+					}
+				}
+
+				if (mistake && !wrongGuess.contains(userGuess)) {
+					countMistakes = countMistakes + 1;
+					printer.printPicture(countMistakes);
+					wrongGuess.add(userGuess);
+
+					if (countMistakes < 8) {
+						System.out.println("That's incorrect! You have " + (8 - countMistakes) + " mistakes left!");
+					}
+				} else if (mistake) {
+					System.out.println("You already tried that letter!");
+				}
+				// System.out.println("Guessed:" + countGuessed);
+				// System.out.println("Mistakes: " + countMistakes);
+			}
 
 			System.out.println("");
-
-			boolean validInput = false;
-			String userGuess2 = " ";
-			
-			while (!validInput) {
-				System.out.println("\n"+"Please enter a letter!");
-				if (!wrongGuess.isEmpty()) {System.out.println("You've already tried: "+wrongGuess);
-				
-				}
-				userGuess2 = sc.next();
-				if ((userGuess2.matches("[A-Za-z]{1}")) && (userGuess2.length() == 1)) {
-					validInput = true;
-				} else {
-					System.out.println("Wrong input!");
-				}
-
-			}
-
-			char userGuess1 = userGuess2.charAt(0);
-			char userGuess = Character.toLowerCase(userGuess1);
-			boolean mistake = true;
-
-			for (i = 0; i < symbols.length; i++) {
-				if ((symbols[i] == userGuess) && (symbols2[i] == userGuess)) {
-					mistake = false;
-				} else if (symbols[i] == userGuess) {
-					symbols2[i] = userGuess;
-					countGuessed = countGuessed + 1;
-					mistake = false;
-					
+			if (countGuessed == word.length()) {
+				System.out.println("Congratulations! You won! :)");
+			} else {
+				System.out.print("Sorry, you lost! :( The word was: ");
+				for (i = 0; i < symbols.length; i++) {
+					System.out.print(symbols[i] + " ");
 				}
 			}
-
-			if (mistake && !wrongGuess.contains(userGuess)) {
-				countMistakes = countMistakes + 1;
-				printer.printPicture(countMistakes);
-				wrongGuess.add(userGuess);
-
-				if (countMistakes < 8) {
-					System.out.println("That's incorrect! You have " + (8 - countMistakes) + " mistakes left!");
-				}
-			} else if (mistake) {
-				System.out.println("You already tried that letter!");
-			}
-			// System.out.println("Guessed:" + countGuessed);
-			// System.out.println("Mistakes: " + countMistakes);
-		}
-
-		System.out.println("");
-		if (countGuessed == word.length()) {
-			System.out.println("Congratulations! You won! :)");
-		} else {
-			System.out.print("Sorry, you lost! :( The word was: ");
-			for (i = 0; i < symbols.length; i++) {
-				System.out.print(symbols[i] + " ");
+			System.out.println("");
+			System.out.println("If you want to play again, please press 'y'");
+			playAgain = false;
+			Scanner sc = new Scanner(System.in);
+			String playAgainAnswer = sc.next();
+			if (playAgainAnswer.contentEquals("y")) {
+				playAgain = true;
 			}
 		}
-
 	}
 
 }
